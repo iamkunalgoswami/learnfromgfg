839ee23ef2bb4073c1697ce75933a84718535283
KunalGoswami@Dell-7390 <kunalgoswami99@gmail.com>
2025-12-09 15:42:50 +0530

    First Commit

---
+++ src/main/java/com/selenophile/App.java
@@ -0,0 +1,16 @@
+package com.selenophile;
+
+import com.selenophile.strings.BasicsOfString;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        new BasicsOfString().declare();
+    }
+}
---
+++ src/main/java/com/selenophile/strings/BasicsOfString.java
@@ -0,0 +1,20 @@
+package com.selenophile.strings;
+
+public class BasicsOfString {
+    /*
+    * Basic Declaration
+    * */
+    public void declare(){
+        String name = new String();
+        printName(name);
+        name = "Kunal";
+        printName(name);
+        String fullName = name.concat(" Goswami");
+        printName(name);
+        printName(fullName);
+
+    }
+    public void printName(String name){
+        System.out.println("My name is "+name);
+    }
+}
